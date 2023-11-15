2e328f08a7c9ca279c3d87c29fb62a9756a2f8d5
samuelabadie <samuel.abadiediez@gmail.com>
2023-11-15 16:36:01 +0100

    add product page + responsive

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,16 +1,36 @@
 // Step 1: Import React
 import * as React from 'react';
 
+import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import HeroImage from '../images/hero-image.png';
 
 // Step 2: Define your component
 const ProductPage = () => {
   return (
     <Layout>
-      <main>
-        <h1>Welcome to my product page !</h1>
-        <p>I'm making this by following the Gatsby Tutorial.</p>
-      </main>
+      <section class="md:flex-row flex flex-col-reverse justify-between gap-x-10 gap-y-10 items-center ">
+        <div class="md:min-w-1/2 w-mr-5">
+          <img src={HeroImage} className="w-full rounded-xl" />
+        </div>
+        <div class="flex flex-col gap-y-6">
+          <h1>spiro</h1>
+          <text class="uppercase">mmi gaming Référence- Ref 06024479</text>
+          <div>
+            <h2 class="text-[50px] font-signika-negative font-black leading-none">14,99€</h2>
+            <div class="mb-8">ou 3x 9,99 avec paypal</div>
+            <button className="inline-block py-1 px-5 rounded-lg uppercase font-semibold bg-secondary-yellow">
+              <Link to="/product">Acheter</Link>
+            </button>
+          </div>
+          <div class="px-2 py-1 bg-red-500 rounded-full justify-center items-center gap-2.5 w-fit inline-block">
+            <div class="text-center text-sm text-white">40% de réduction pout l’achat d’un deuxième spiro</div>
+          </div>
+          <p class="md:max-w-[500px]">
+          Jeu de société de fou qui viendra éduquer vos enfants et vous permettra de prendre un maximum de plaisir avec eux. Découvrez qui à été le  coupable à travers des parties aussi amusantes qu’originales
+          </p>
+        </div>
+      </section>
     </Layout>
   );
 };
