f41ba3a71ba148c9f60f4440da9a44695b2da6c8
nin0 <95708793+nin0dev@users.noreply.github.com>
2023-11-16 10:15:00 +0100

    feat: mobile menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,8 +12,50 @@ const Header = () => {
           <img className="text-white" src={Logo} alt="Logo" />
         </Link>
       </div>
+      {/* Desktop Menu */}
       <div>
-        <ul className="flex gap-5">
+        <ul className="hidden md:flex md:gap-5">
+          <li>
+            <Link to="/">Accueil</Link>
+          </li>
+          <li>
+            <Link to="/product">Acheter</Link>
+          </li>
+          <li>
+            <Link to="/rules">Règles</Link>
+          </li>
+          <li>
+            <Link to="/team">Équipe</Link>
+          </li>
+        </ul>
+      </div>
+      {/* Mobile Menu */}
+      <div>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span class="sr-only">Open main menu</span>
+          <svg
+            class="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <ul id="mobileList" className="flex flex-col md:hidden">
           <li>
             <Link to="/">Accueil</Link>
           </li>
